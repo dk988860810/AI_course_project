f98688d852309f342081f9d8c7a4cff2c309c680
dk988860810 <dk98860810@gmail.com>
2024-04-28 12:56:58 +0800

    modified:   static/call_cancel.html 	modified:   static/checking.html 	modified:   static/js/get_image_info.js

--- static/js/get_image_info.js
+++ static/js/get_image_info.js
@@ -2,12 +2,21 @@
 const urlParams = new URLSearchParams(window.location.search);
 const clickedImage = urlParams.get('clickedImage');
 const imageInfoMap = {
-    "1F_Area_A":"1樓A區",
-    "1F_Area_B":"1樓B區",
-    "1F_Area_C":"1樓C區",
-}
+    "1F_Area_A": "1樓A區",
+    "1F_Area_B": "1樓B區",
+    "1F_Area_C": "1樓C區",
+};
 
-const clickedImageInfo = imageInfoMap[clickedImage] || 'test'
+// 创建新的 URL
+const url_clickedImage = "call_cancel.html?clickedImage=" + clickedImage;
+
 // 显示被点击的图片信息
-const clickedImageInfoElement = document.getElementById('clickedImageInfo');
-clickedImageInfoElement.textContent = clickedImageInfo;
+document.addEventListener("DOMContentLoaded", function() {
+    const clickedImageInfoElement = document.getElementById('clickedImageInfo');
+    const clickedImageInfo = imageInfoMap[clickedImage] || '测试';
+    clickedImageInfoElement.textContent =  clickedImageInfo;
+
+    // 将点击按钮的 href 设置为动态生成的 URL
+    const checkButton = document.getElementById('checkButton');
+    checkButton.href = url_clickedImage;
+});
