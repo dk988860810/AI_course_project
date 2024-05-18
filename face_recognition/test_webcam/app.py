7b4bd13b4c0357e5b2c3df13c7aaaae947a9be30
shihgggg <168054407+shihgggg@users.noreply.github.com>
2024-05-18 10:09:44 +0800

    Add files via upload

--- face_recognition/test_webcam/app.py
+++ face_recognition/test_webcam/app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/hello')
+def hello():
     return render_template('hello.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    # 确保在启动应用之前定义所有路由
+    app.run(host='0.0.0.0', port=5000)
