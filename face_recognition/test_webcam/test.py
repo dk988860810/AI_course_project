5d486ae6e50562cbedebe713593a3df088dc1671
shihgggg <168054407+shihgggg@users.noreply.github.com>
2024-05-24 10:17:54 +0800

    Add files via upload

--- face_recognition/test_webcam/test.py
+++ face_recognition/test_webcam/test.py
@@ -1,7 +1,6 @@
-# os.makedirs(current_face_dir)
 import os
 
-# 假设 current_face_dir 已经通过上面的代码创建了
-new_subfolder = os.path.join('123', "123")
-print(new_subfolder)
-os.makedirs(new_subfolder, exist_ok=True)
+# 定义路径
+
+# 创建嵌套目录，确保所有父目录存在
+os.makedirs('~/AI_course_project/face_recognition/data/data_faces_from_camera/123')
