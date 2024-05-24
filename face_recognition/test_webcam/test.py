dca62aadc3c5093d422da7f79367c4f1065a315b
shihgggg <168054407+shihgggg@users.noreply.github.com>
2024-05-24 10:12:06 +0800

    Add files via upload

--- face_recognition/test_webcam/test.py
+++ face_recognition/test_webcam/test.py
@@ -2,5 +2,6 @@
 import os
 
 # 假设 current_face_dir 已经通过上面的代码创建了
-new_subfolder = os.path.join('', "123")
+new_subfolder = os.path.join('123', "123")
+print(new_subfolder)
 os.makedirs(new_subfolder, exist_ok=True)
